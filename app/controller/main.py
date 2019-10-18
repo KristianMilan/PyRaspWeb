d5f72c264e9dc17097772b5ee1ce00b337ba6b46
juniortada <juniortada@gmail.com>
2019-10-18 19:54:38 -0300

    [fix] add delete funciton

--- app/controller/main.py
+++ app/controller/main.py
@@ -6,6 +6,7 @@ from platform import python_version
 import os
 import json
 
+path = f'{os.getcwd()}/app/data/db.json'
 
 @app.route('/')
 @app.route('/index')
@@ -40,17 +41,16 @@ def backup():
 def config():
 	if request.method == 'POST':	
 		try:
-			path = f'{os.getcwd()}/app/data/db.json'
 			with open(path, 'r+') as file:
 				data = json.load(file)
 				data['schemas'].append(request.get_json())
 				file.seek(0)
 				json.dump(data, file, indent=4)
 				file.truncate()
-			m = 'Save with Success'
+			m = 'Save Success'
 			msg = {'value': m, 'status': 'SUCCESS'}
 		except Exception as e:
-			m = f'Error: {e}'
+			m = f'Save {e}'
 			msg = {'value': m, 'status': 'ERROR'}
 		finally:
 			return jsonify(return_msg=msg)
@@ -59,10 +59,37 @@ def config():
 
 @app.route('/detail/<int:id>')
 def detail(id):
-    return render_template('detail.html')
+    data = None
+    try:
+        path = f'{os.getcwd()}/app/data/db.json'
+        with open(path) as file:
+            data = json.load(file)
+            data = data['schemas'][int(id)]
+            data['id'] = id
+    except Exception as e:
+        data = [{'error': e}]
+    finally:
+        return render_template('detail.html', data=data)
 
-@app.route('/_save', methods=['GET', 'POST'])
-def _save():
-	import pdb
-	pdb.set_trace()
-	return redirect(url_for('index'))
+@app.route('/_delete', methods=['POST'])
+def _delete():
+    data = None
+    try:
+        id = int(request.get_data().decode('utf-8').replace('id=',''))
+        if id >= 0:
+            m = 'Delete Sucess'
+            msg = {'value': m, 'status': 'SUCCESS'}
+            with open(path, 'r+') as file:
+                data = json.load(file)
+                data['schemas'].pop(id)
+                file.seek(0)
+                json.dump(data, file, indent=4)
+                file.truncate()
+        else:
+            m = 'Error: Invalid ID!'
+            msg = {'value': m, 'status': 'ERROR'}
+    except Exception as e:
+        m = f'Delete {e}'
+        msg = {'value': m, 'status': 'ERROR'}
+    finally:
+        return jsonify(return_msg=msg)
